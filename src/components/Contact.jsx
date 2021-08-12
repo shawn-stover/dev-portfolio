eb201031e62e42ec310f45c004b1f9ddeef7520e
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-08-12 05:39:00 -0700

    Finished Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 // Imports
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 import { Helmet } from 'react-helmet'
 
 // Contact Component
@@ -8,7 +9,52 @@ export default function Contact() {
             <Helmet>
                 <title>Shawn's Portfolio | Contact</title>
             </Helmet>
-            <h1>Contact</h1>
+            <div className='contact-body'>
+                <Grid className='contact-grid'>
+                    <Cell col={6}>
+                        <img className='avatar'
+                            src='./img/ShawnPic.png'
+                            alt='Shawn' 
+                        />
+                        <h2>Shawn Stover</h2>
+                        <p style={{width: '75%', fontSize: '3.2vh', margin: 'auto', paddingTop: '1em'}}>
+                            Feel free to ask me about any of the projects on the Projects page, my current projects and skill-building exercises, or simply to touch base with me at any of the contact points listed here.
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+                        <div className='contact-list'>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '4.75vh', fontFamily: 'Anton', color: '#FFFFFF'}}>
+                                        <i className='fas fa-phone-square' aria-hidden='true' />
+                                        (951) 880-5982
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '4.75vh', fontFamily: 'Anton', color: '#FFFFFF'}}>
+                                        <i className='fas fa-fax' aria-hidden='true' />
+                                        (951) 845-2865
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '4.75vh', fontFamily: 'Anton', color: '#FFFFFF'}}>
+                                        <i className='fas fa-envelope' aria-hidden='true' />
+                                        shawn.stover.it.cs@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '4.75vh', fontFamily: 'Anton', color: '#FFFFFF'}}>
+                                        <i className='fab fa-slack' aria-hidden='true' />
+                                        shawn.stover.it.cs@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>    
+                    </Cell>
+                </Grid>
+            </div>
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 // Imports
 import { useState } from 'react'
-import { Tab , Tabs, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl'
+import { Tab , Tabs, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu } from 'react-mdl'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
@@ -17,7 +17,7 @@ export default function Projects() {
             return(
                 <div className='projects-grid'>
                     <Card className='card' shadow={5}>
-                        <CardTitle style={{color: '#000000', height: '25vh', background: 'url(https://media.giphy.com/media/HroASiJhtF9ZrZ7kYI/giphy.gif) center / cover'}}>Survival Chess</CardTitle>
+                        <CardTitle style={{color: '#000000', height: '33vh', background: 'url(https://media.giphy.com/media/HroASiJhtF9ZrZ7kYI/giphy.gif) center / cover'}}>Survival Chess</CardTitle>
                         <CardText>
                             Survival Chess used HTML / CSS and Javascript. It is chess with a twist. It is written in Javascript and uses the chessboard.js and chess.js libraries to control basic game logic and functions. The player plays against never ending computers and their board state is preserved between games until they lose.
                         </CardText>
@@ -29,8 +29,7 @@ export default function Projects() {
                                 <Link to={{ pathname: 'https://shawn-stover.github.io/Survival-Chess/' }} target='_blank' style={{textDecoration: 'none'}}>Live Site</Link>
                             </Button>
                         </CardActions>
-                        <CardMenu style={{color: '#ffffff'}}>
-                            <IconButton name='share' />
+                        <CardMenu style={{color: '#FFFFFF'}}>
                         </CardMenu>
                     </Card>
                 </div>    
@@ -39,7 +38,7 @@ export default function Projects() {
             return(
                 <div className='projects-grid'>
                     <Card className='card' shadow={5}>
-                        <CardTitle style={{color: '#ffffff', height: '176px', background: 'url(https://media.giphy.com/media/0y3wefCTDjA1WdeWft/giphy.gif) center / cover'}}>Final Fantasy 14 Note App</CardTitle>
+                        <CardTitle style={{color: '#000000', height: '33vh', background: 'url(https://media.giphy.com/media/0y3wefCTDjA1WdeWft/giphy.gif) center / cover'}}>Final Fantasy 14 Note App</CardTitle>
                         <CardText>
                             Final Fantasy 14 Note App uses EJS, NodeJs and SQL. It was written so that users can search their characters and keep character advancement notes and tasks separated by class. This way users can stay organized.
                         </CardText>
@@ -51,8 +50,7 @@ export default function Projects() {
                                 <Link to={{ pathname: 'https://shawn-stover.github.io/Survival-Chess/' }} target='_blank' style={{textDecoration: 'none'}}>Live Site</Link>
                             </Button> */}
                         </CardActions>
-                        <CardMenu style={{color: '#ffffff'}}>
-                            <IconButton name='share' />
+                        <CardMenu style={{color: '#FFFFFF'}}>
                         </CardMenu>
                     </Card>
                 </div>
@@ -61,7 +59,7 @@ export default function Projects() {
             return(
                 <div className='projects-grid'>
                     <Card className='card' shadow={5}>
-                        <CardTitle style={{color: '#000000', height: '176px', background: 'url(https://media.giphy.com/media/MvdNxKTCzTLv7O9wOh/giphy.gif) center / cover'}}>Falendar</CardTitle>
+                        <CardTitle style={{color: '#000000', height: '33vh', background: 'url(https://media.giphy.com/media/MvdNxKTCzTLv7O9wOh/giphy.gif) center / cover'}}>Falendar</CardTitle>
                         <CardText>
                             Falendar was written as a MERN Stack collaboration project not only as a group of four students from my class, but a pair of UX/UI Designers from another class as well. The app was written as a way for families to begin to plan their lives back out again post pandemic and allow them to make their scheduling easier.
                         </CardText>
@@ -73,13 +71,12 @@ export default function Projects() {
                                 <Link to={{ pathname: 'https://github.com/EllieLissner/Collab-MERN-Client' }} target='_blank' style={{textDecoration: 'none'}}>GH Client</Link>
                             </Button>
                         </CardActions>
-                        <CardMenu style={{color: '#ffffff'}}>
-                            <IconButton name='share' />
+                        <CardMenu style={{color: '#FFFFFF'}}>
                         </CardMenu>
                     </Card>
 
                     <Card className='card' shadow={5}>
-                        <CardTitle style={{color: '#000000', height: '176px', background: 'url(https://media.giphy.com/media/KNy3Z6MmzaDk9hnjyL/giphy.gif'}}>Final Fantasy 14 Note App Refactor</CardTitle>
+                        <CardTitle style={{color: '#000000', height: '33vh', background: 'url(https://media.giphy.com/media/KNy3Z6MmzaDk9hnjyL/giphy.gif'}}>Final Fantasy 14 Note App Refactor</CardTitle>
                         <CardText>
                             This was my final project during my bootcamp. In this project, I took the ap written in EJS, NodeJs and SQL and have refactored it into a MERN stack app controlled by a backend Mongo database that serves a frontend React client for users. This app includes authentication that the EJS app did not, and a much better looking website and file structure.
                         </CardText>
@@ -91,8 +88,7 @@ export default function Projects() {
                             <Link to={{ pathname: 'https://github.com/shawn-stover/final-fantasy-14-app-refactor-client' }} target='_blank' style={{textDecoration: 'none'}}>GH Client</Link>
                             </Button>
                         </CardActions>
-                        <CardMenu style={{color: '#ffffff'}}>
-                            <IconButton name='share' />
+                        <CardMenu style={{color: '#FFFFFF'}}>
                         </CardMenu>
                     </Card>
                 </div>
@@ -101,17 +97,16 @@ export default function Projects() {
             return(
                 <div className='projects-grid'>
                     <Card className='card' shadow={5}>
-                        <CardTitle  style={{color: '#ffffff', height: '25vh', background: 'url(https://media.giphy.com/media/MOyHcpdL4zIFf1YUCt/giphy.gif) center / cover'}}>Space Invaders Clone</CardTitle>
+                        <CardTitle  style={{color: '#FFFFFF', height: '33vh', background: 'url(https://media.giphy.com/media/MOyHcpdL4zIFf1YUCt/giphy.gif) center / cover'}}>Space Invaders Clone</CardTitle>
                         <CardText>
-                            Space Invaders Clone was a personal project I did for skill building after my bootcamp. I am still cleaning up bugs, but the game is functional. You can clone down the repo and run the code in an IDE to play the game.
+                            Space Invaders Clone was a personal project I did for skill building after my bootcamp. I am still cleaning up bugs, but the game is functional. You can clone down the repo and run the code in a Development Environment to play the game.
                         </CardText>
                         <CardActions border>
                             <Button colored>
                                 <Link to={{ pathname: 'https://github.com/shawn-stover/Space_Invaders_Clone_Python3' }} target='_blank' style={{textDecoration: 'none'}}>Github</Link>
                             </Button>
                         </CardActions>
-                        <CardMenu style={{color: '#ffffff'}}>
-                            <IconButton name='share' />
+                        <CardMenu style={{color: '#FFFFFF'}}>
                         </CardMenu>
                     </Card>
                 </div>    
@@ -130,7 +125,7 @@ export default function Projects() {
                 <Tab>MERN</Tab>
                 <Tab>Python</Tab>
             </Tabs>
-            <Grid classname='projects-grid'>
+            <Grid >
                 <Cell col={12}>
                     <div className='content'>
                         {toggleCategories()}
