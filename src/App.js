13f07e4b08f506ce0dbd4798ec84bfb7c0969a8c
shawn-stover <shawn-stover@github.com>
2021-10-11 12:40:40 -0700

    Update Google Analytics
    Updated Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@
 import './App.css'
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
 import Routes from './components/Routes.jsx'
+import ReactGA from 'react-ga'
+import ReactChangeTracker from 'ReactChangeTracker.js'
 import { Link } from 'react-router-dom'
+
+const TRACKING_ID = process.env.REACT_APP_TRACKING_ID
+ReactGA.initialize(TRACKING_ID, {
+  debug: true,
+  titleCase: false,
+})
 
 // App template using react-mdl
 export default function App() {
