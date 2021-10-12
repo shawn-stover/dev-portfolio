ae20d81f0496e4f274d26bf1de5b594c09b1a54f
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-10-12 10:17:56 -0700

    Google Analytics Functionality
    Implement Google Analytics Functionality

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,17 +1,38 @@
 // Imports
-import { Switch, Route } from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
 import Landing from './Landing.jsx'
 import About from './About.jsx'
 import Projects from './Projects.jsx'
 import Contact from './Contact.jsx'
 
+// Create History Variable
+const history = createBrowserHistory()
+
+// Initialize Google Analytics Tracker
+const TRACKING_ID = process.env.REACT_APP_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+// Initialize pageview tracking
+history.listen(location => {
+  
+  // Update current page
+  ReactGA.set({ page: location.pathname })
+
+  // Record a page view
+  ReactGA.pageview(location.pathname)
+})
+
 export default function Routes() { 
     return(
-        <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route path='/about' component={About} />
-            <Route path='/projects' component={Projects} />
-            <Route path='/contact' component={Contact} />
-        </Switch>
+        <Router history={history}>
+            <Switch>
+                <Route exact path='/' component={Landing} />
+                <Route path='/about' component={About} />
+                <Route path='/projects' component={Projects} />
+                <Route path='/contact' component={Contact} />
+            </Switch>
+        </Router>
     )
 }    
